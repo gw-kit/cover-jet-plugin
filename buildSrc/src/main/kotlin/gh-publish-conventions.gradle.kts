536aa3577ef9ec6aa66affbe209e739f0f190634
Sergii Gnatiuk <surpsg2012@gmail.com>
2025-03-02 12:01:24 +0200

    gh pkg publishing (#3)
    * gh pkg publishing

---
+++ buildSrc/src/main/kotlin/gh-publish-conventions.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    `maven-publish`
+}
+
+if (rootProject.hasProperty("snapshotPrefix")) {
+    val snapshotPrefix: String by project
+    version = "$version-SNAPSHOT.${snapshotPrefix}"
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+        maven {
+            name = "GhPackages"
+            url = uri("https://maven.pkg.github.com/gw-kit/cover-jet-plugin")
+            credentials {
+                username = System.getenv("GH_USER")
+                password = System.getenv("GH_TOKEN")
+            }
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
+    }
+}
