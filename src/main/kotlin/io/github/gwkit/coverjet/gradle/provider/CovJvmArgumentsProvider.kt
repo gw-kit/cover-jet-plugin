a5b5fd5f43a53a515201b243aeed09d3cccfd200
Sergii Gnatiuk <surpsg2012@gmail.com>
2025-03-03 00:09:27 +0200

    expose additional cov agent args for the configuration

--- src/main/kotlin/io/github/gwkit/coverjet/gradle/provider/CovJvmArgumentsProvider.kt
+++ src/main/kotlin/io/github/gwkit/coverjet/gradle/provider/CovJvmArgumentsProvider.kt
@@ -1,13 +1,14 @@
 package io.github.gwkit.coverjet.gradle.provider
 
+import io.github.gwkit.coverjet.gradle.CoverJetExtension
 import io.github.gwkit.coverjet.gradle.task.CovAgentProperties
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.process.CommandLineArgumentProvider
 import java.io.File
-import javax.inject.Inject
 
-internal class CovJvmArgumentsProvider @Inject constructor(
+internal class CovJvmArgumentsProvider(
+    private val extension: CoverJetExtension,
     private val covAgentProvider: Provider<File>,
     private val agentPropertiesProvider: TaskProvider<CovAgentProperties>
 ) : CommandLineArgumentProvider {
@@ -17,15 +18,21 @@ internal class CovJvmArgumentsProvider @Inject constructor(
             .zip(agentPropertiesProvider.propsFilePath) { agentJar, propFile ->
                 listOf(
                     "-javaagent:${agentJar}=${propFile}",
-                    ENABLE_TRACING.jvmProp,
+                    enableTracingProperty.jvmProp,
                     PRINT_ONLY_ERRORS.jvmProp,
                     IGNORE_STATIC_CONSTRUCTORS.jvmProp,
-                    DO_NOT_COUNT_HIT_AMOUNT.jvmProp,
+                    countHitsProperty.jvmProp,
                 )
             }
             .get()
             .toMutableList()
     }
+
+    private val enableTracingProperty: String
+        get() = "$ENABLE_TRACING_PROP_NAME=${extension.enableTracing.get()}"
+
+    private val countHitsProperty: String
+        get() = "$DO_NOT_COUNT_HIT_AMOUNT_PROP_NAME=${extension.countHits.get()}"
 
     private val Provider<File>.agentFilePath: Provider<String>
         get() = map { it.absolutePath }
@@ -37,9 +44,10 @@ internal class CovJvmArgumentsProvider @Inject constructor(
         get() = "-D$this"
 
     companion object {
-        private const val ENABLE_TRACING = "idea.new.tracing.coverage=true"
+        private const val ENABLE_TRACING_PROP_NAME = "idea.new.tracing.coverage"
+        private const val DO_NOT_COUNT_HIT_AMOUNT_PROP_NAME = "idea.coverage.calculate.hits"
+
         private const val PRINT_ONLY_ERRORS = "idea.coverage.log.level=error"
         private const val IGNORE_STATIC_CONSTRUCTORS = "coverage.ignore.private.constructor.util.class=true"
-        private const val DO_NOT_COUNT_HIT_AMOUNT = "idea.coverage.calculate.hits=false"
     }
 }
